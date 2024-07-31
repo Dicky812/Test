670ed8165d902006a5949b372f6cc79b6a61785c
Dicky812 <m_ardiansyah_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 16:43:55 +0700

    Add files via upload

---
+++ pancet.java
@@ -0,0 +1,9 @@
+public class pancet {
+    public static void main(String[] args) {
+        String nama = "mochamad dicky ardiansyah";
+        String kelas = "X RPL 6";
+        String smp = "smp al ikhlash lumajang";
+        String nohp = "087794126574";
+        System.out.println(nama+kelas+smp+nohp);
+    }
+}
